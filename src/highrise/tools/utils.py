22538d75fe8d77fae0ccd3721f144c4e21b7488a
wk91p <ahmedismael197743@gmail.com>
2026-08-30 07:25:54 +0300

    feat(utils): add format_time helper to Util class

--- src/highrise/tools/utils.py
+++ src/highrise/tools/utils.py
@@ -18,3 +18,22 @@ class Utils:
                 amount -= value
 
         return result
+
+    @staticmethod
+    def format_time(seconds: float) -> str:
+        """Format time from raw seconds to human readable format (`4d 14h 40m 12s`)"""
+
+        seconds = int(seconds)
+
+        seconds_formatted = seconds % 60
+        minutes = (seconds // 60) % 60
+        hours = (seconds // 3600) % 24
+        days = seconds // 86400
+
+        parts = []
+        if days > 0: parts.append(f"{days}d")
+        if hours > 0: parts.append(f"{hours}h")
+        if minutes > 0: parts.append(f"{minutes}m")
+        if seconds_formatted > 0 or len(parts) == 0: parts.append(f"{seconds_formatted}s")
+
+        return ", ".join(parts)
