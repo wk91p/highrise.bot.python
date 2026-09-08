cb4cb149bb9088b6ea4b402c3bc455b1978ae6a9
wk91p <ahmedismael197743@gmail.com>
2026-09-08 20:18:10 +0300

    removing Cache from __init__

--- src/highrise/__init__.py
+++ src/highrise/__init__.py
@@ -7,5 +7,4 @@ from .tools.roles import Roles
 from .configs import BotConfig, ConnectionConfig, LoggerConfig, AutoFetchConfig, RolesConfig
 from .tools.utils import Utils
 from .webapi import WebApi
-from .cache.cache import Cache
 from .tools.loop_task import LoopTask
